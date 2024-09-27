d485d3c87afe92a07a5c69e810cd956c7d7da07c
tsimpson97 <150633584+tsimpson97@users.noreply.github.com>
2024-09-26 21:37:37 -0400

    Add files via upload

---
+++ Module 6 assignment-Pyton IDLE.py
@@ -0,0 +1,8 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> c = "cats"
+>>> d = "dogs."
+>>> s = "It is raining "
+>>> a = s + c + " and " + d
+>>> print(a)
+It is raining cats and dogs.
